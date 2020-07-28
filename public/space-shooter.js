07315aa43364dcd2e24e2d3e8ac4759b86b93d7e
nest-24 <nest-24@nomins-iMac.local>
2020-07-28 15:10:06 +0800

    nis

--- public/space-shooter.js
+++ public/space-shooter.js
@@ -5,16 +5,35 @@ const back = new Image;
 back.src = "space-cut.jpg";
 const ship = new Image;
 ship.src = "shooter.png";
-
-let x;
-let y;
+cs.width = innerWidth;
+cs.height  = innerHeight;
+let now;
+if(window.innerHeight < window.innerWidth){
+    now = true;
+}
+else{
+    now = false;
+}
+let x=0;
+let y=0;
 const draw = () =>{
     ctx.clearRect(0, 0, cs.width, cs.height);
-    ctx.drawImage(back, 0, 0, cs.width, cs.height);
-    ctx.drawImage(ship, x , y+100, 100, 100);
-
+    ctx.drawImage(back, 0, y, cs.width, cs.height);
+    ctx.drawImage(ship, x-50 , y-100, 100, 100);
 }
 const a = setInterval(() => {
+    if(window.innerHeight < window.innerWidth && now==false){
+        location.reload();
+    }
+    if(window.innerHeight > window.innerWidth && now==true){
+        location.reload();
+    }
+    // if(now == true){
+    //     x -= 3;
+    // }
+    // else{
+    //     y -= 3;
+    // }
     draw();
 }, 30)
 function printMousePos(event) {
@@ -23,7 +42,15 @@ function printMousePos(event) {
     y =event.clientY;
     ctx.clearRect(0, 0, cs.width, cs.height);
     ctx.drawImage(back, 0, 0, cs.width, cs.height);
-    ctx.drawImage(ship, x, y+100, 50, 20);
+    ctx.drawImage(ship, x-50 , y-100, 100, 100);
 
 }
-document.addEventListener("mousedown", printMousePos);
+document.addEventListener("mousemove", printMousePos);
+document.addEventListener("touchmove", touch = (e) => {
+    x = e.touches[0].clientX;
+    y = e.touches[0].clientY;
+    console.log(x);
+});
+window.onresize = () => {
+    location.reload();
+}
